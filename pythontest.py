a258c0b1eb1b152af5425006ef4879c3b5581976
Ameyanagi <ryuichi@ameyanagi.com>
2024-02-28 12:21:52 -0500

    refactor

--- pythontest.py
+++
@@ -1,8 +0,0 @@
-from larch.math import utils
-import numpy as np
-
-spectrum = np.loadtxt("tests/testfiles/Ru1 RS0001-1 CpstarRu2 Ru  0001.dat")
-
-print(utils.smooth(spectrum[:,0], np.log(spectrum[:,1]/spectrum[:,2])))
-
-print("Hello World!")
